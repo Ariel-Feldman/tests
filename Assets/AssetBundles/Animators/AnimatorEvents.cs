24cc5167858f3fe438f0ecde829cfb3c8134cbe0
Ariel <arielfel@gmail.com>
2022-09-07 01:36:06 +0300

    Animator Events working!!

--- Assets/AssetBundles/Animators/AnimatorEvents.cs
+++ Assets/AssetBundles/Animators/AnimatorEvents.cs
@@ -1,24 +1,58 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimatorEvents : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private List<string> _states;
     
+    private List<int> _statesHash = new();
     
     private void Awake()
     {
+        foreach (var state in _states)
+        {
+            var stateHash = Animator.StringToHash(state);
+            _statesHash.Add(Animator.StringToHash(state));
+            Debug.Log($"State Name: {state} State Hash: {stateHash}");
+        }
+        
         AnimatorStateMachine.OnAnimatorStateEnter += OnStateEnter;
         AnimatorStateMachine.OnAnimatorStateExit += OnStateExit;
     }
 
-    public void OnStateEnter(AnimatorStateInfo animatorStateInfo)
+    public void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo)
     {
-        Debug.Log($"qqq Animator state hash code enter: {animatorStateInfo}");
-        Debug.Log($"qqq Animator state name is SomeState?: {animatorStateInfo.IsName("SomeState")}");
-        
+        Debug.Log($"Is my animator Animator?: {_animator == animator} State hash code enter: {animatorStateInfo.shortNameHash}");
     }
     
-    public void OnStateExit(AnimatorStateInfo animatorStateInfo)
+    public void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo)
     {
-        Debug.Log($"qqq Animator state hash code exit: {animatorStateInfo}");
+        Debug.Log($"Is my animator Animator?: {_animator == animator} State hash code exit: {animatorStateInfo.shortNameHash}");
+    }
+
+    public void OnTransitionInStarted()
+    {
+    }
+    
+    public void OnTransitionInEnded()
+    {
+    }
+    
+    public void OnTransitionOutStarted()
+    {
+    }
+    
+    public void OnTransitionOutEnded()
+    {
+    }
+    
+    
+    public void OnTransitionIdleStarted()
+    {
+    }
+    
+    public void OnTransitionIdleEnded()
+    {
     }
 }
